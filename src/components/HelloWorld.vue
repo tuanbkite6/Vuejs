9c01dfb4bcb53f956caf4e942723fa5dedba6383
Ta Huy Tuan <tahuytuan@cityascom.vn>
2024-03-12 16:45:58 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <p>Click A {{ a }} times</p>
+    <p>Click B {{ b }} times</p>
+    <button v-on:mouseover="clickA()">Click A</button>
+    <button v-on:click="clickB()">ClickB</button>
+    <th>
+      <tr>
+        Name
+      </tr>
+      <tr>
+        <input type="text" id="Name" v-model="name" />
+      </tr>
+    </th>
+    <th>
+      <tr>
+        age
+      </tr>
+      <tr>
+        <input type="text" id="age" v-model="c" />
+      </tr>
+    </th>
+    <th>
+      <tr>
+        Job
+      </tr>
+      <tr>
+        <input type="text" id="Job" v-model="job" />
+      </tr>
+    </th>
+    <th>
+      <tr>
+        Gender
+      </tr>
+      <tr>
+        <select v-model="gender" name="gender" id="">
+          <option value="1" selected disabled>
+            -----Select your gender-------
+          </option>
+          <option value="2">Male</option>
+          <option value="3">Female</option>
+          <option value="4">Other</option>
+        </select>
+        <button type="submit" v-on:click = "submitForm()">Submit</button>
+      </tr>
+    </th>
+  </div>
+  <h1 v-if="gender == null">Welcome to testing</h1>
+  <h1 v-else-if="gender == 2">You is perfect man</h1>
+  <h1 v-if="gender == 3">Are you really woman?</h1>
+  <h1 v-if="gender == 4">You can join LGPT community</h1>
+</template>
+
+<script>
+import InformationList from "./InformationList.vue";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      a: 0,
+      b: 0,
+      c: null,
+      users: [],
+    };
+  },
+  component: {
+    InformationList,
+  },
+  methods: {
+    clickA: function () {
+      console.log("Call A method");
+      return this.a++;
+    },
+    clickB: function () {
+      console.log("Call B method");
+      return this.b++;
+    },
+    addUser: function (newUser) {
+      this.users.push(newUser);
+    },
+    submitForm() {
+      const newUser = {
+        name: "New User",
+        age: 0,
+        job: "Unknown",
+        gender: "Other",
+      }
+      this.addUser(newUser);
+      console.log("Form submitted")
+      console.log(this.users)
+    },
+}
+}
+</script>
