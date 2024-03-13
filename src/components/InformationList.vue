6fac62c46d9c760494467301d5137f97ad186930
Ta Huy Tuan <tahuytuan@cityascom.vn>
2024-03-13 16:56:55 +0700

    backup informationlist

---
+++ src/components/InformationList.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+      <div class="row">
+          <table border="1">
+              <tr>
+                      <th>User name</th>
+                      <th>Age</th>
+                      <th>Job</th>
+                      <th>Gender</th>
+                  </tr>
+                  <tr class="list" v-for="user in users" :key="user.NAME">
+                      <td>{{ user.name }}</td>
+                      <td>{{ user.age }}</td>
+                      <td>{{ user.job }}</td>
+                      <td> {{ user.gender }}</td>
+                  </tr>
+          </table>
+      </div>
+  
+    </section>
+  </template>
+  <script>
+  export default {
+    name: "InformationList",
+    props : {
+      users : Array
+    },
+        
+  };
+  </script>
